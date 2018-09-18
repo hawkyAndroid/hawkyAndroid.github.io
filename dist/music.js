82a0eba32c392a2f78f0d1b046574d4d005b94d1
Leo Liu <1683177866@qq.com>
2018-09-18 19:47:07 +0800

    Site updated: 2018-09-18 19:47:01

---
+++ dist/music.js
@@ -0,0 +1,25 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD����֮��",
+        artist: '����Բ',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '9420',
+        artist: '��С��',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '������',
+        artist: '������',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
