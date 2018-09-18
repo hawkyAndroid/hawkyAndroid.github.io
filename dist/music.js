140f5faaae694067ff4232c217ac22c762d7bc20
Leo Liu <1683177866@qq.com>
2018-09-18 19:55:23 +0800

    Site updated: 2018-09-18 19:55:21

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
         name: "I Miss You",
         artist: 'Czarina',
         url: 'http://fs.w.kugou.com/201809181831/08a74580fe5f30932c8aaf2e1f170813/G013/M06/04/11/TQ0DAFULdt-AfwSvADgNY3dhOQc093.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        cover: 'http://imge.kugou.com/stdmusic/240/20150718/20150718044549325738.jpg',
       },
       {
         name: '9420',
